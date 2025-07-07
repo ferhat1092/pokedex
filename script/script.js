ee60bc203af5a92686c9ccdedcbb244219935f39
Ferhat Doganci <ferhat.doganci@icloud.com>
2025-07-08 01:32:30 +0200

    fetch all url and get all details

--- script/script.js
+++ script/script.js
@@ -4,18 +4,23 @@ function init(){
 
 async function getPokemons() {
     let responsPokemons = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0');
-    let pokemonsObj = await responsPokemons.json()
-    let pokemonsNameArr  = pokemonsObj.results;
-    render(pokemonsNameArr);
+    let pokemonsObj = await responsPokemons.json();
+    let detailPromises = pokemonsObj.results.map(async (pokemon) => (await fetch(pokemon.url)).json());
+    let detailsAboutPokemons = await Promise.all(detailPromises);
+    render(detailsAboutPokemons);
 };
 
-function render(pokemonsNameArr) {
+function render(detailsAboutPokemons) {
     let mainContainerContentRef = document.getElementById('main_container');
-    mainContainerContentRef.innerHTML = pokemonsNameArr.map((pokemon) => pokeCardsTemplate(pokemon)).join();
+    mainContainerContentRef.innerHTML = detailsAboutPokemons.map((pokemon) => pokeCardsTemplate(pokemon)).join('');
 };
 
 function pokeCardsTemplate(pokemon) {
       return `
-               <div class="poke-card">${pokemon.name}</div>
+               <div class="poke-card">
+               <h3>${pokemon.name.toUpperCase()}</h3>
+               <img src="${pokemon.sprites.front_default}" alt="pokemon-pic">
+               <p>${pokemon.types[0].type.name}</p>
+               </div>
       `;
 };

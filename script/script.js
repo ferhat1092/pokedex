049f0ed80a5e35c1a327eb73b0f5ad2a1e50ef93
Ferhat Doganci <ferhat.doganci@icloud.com>
2025-07-07 23:10:08 +0200

    do an array with the poke names

--- script/script.js
+++ script/script.js
@@ -4,13 +4,14 @@ function init(){
 
 async function getPokemons() {
     let responsPokemons = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0');
-    let pokemonsArr = [await responsPokemons.json()]
-    render(pokemonsArr);
+    let pokemonsObj = await responsPokemons.json()
+    let pokemonsNameArr  = pokemonsObj.results;
+    render(pokemonsNameArr);
 };
 
-function render(pokemonsArr) {
+function render(pokemonsNameArr) {
     let mainContainerContentRef = document.getElementById('main_container');
-    mainContainerContentRef.innerHTML = pokemonsArr.map((pokemon) => pokeCardsTemplate(pokemon)).join();
+    mainContainerContentRef.innerHTML = pokemonsNameArr.map((pokemon) => pokeCardsTemplate(pokemon)).join();
 };
 
 function pokeCardsTemplate(pokemon) {

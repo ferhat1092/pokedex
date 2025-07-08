72c2beb27c45a2c762b430dec4bce7d5fdeefdd9
Ferhat Doganci <ferhat.doganci@icloud.com>
2025-07-09 00:20:51 +0200

    add new button to get 20 more pokemons

--- script/script.js
+++ script/script.js
@@ -1,25 +1,34 @@
-function init(){
+let currentOffset = 0;
+
+
+function init() {
     getPokemons();
 };
 
 async function getPokemons() {
-    let responsPokemons = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0');
+    let responsPokemons = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${currentOffset}`);
     let pokemonsObj = await responsPokemons.json();
     let detailPromises = pokemonsObj.results.map(async (pokemon) => (await fetch(pokemon.url)).json());
     let detailsAboutPokemons = await Promise.all(detailPromises);
     render(detailsAboutPokemons);
 };
 
+function getMorePokemons() {
+    currentOffset += 20;
+    getPokemons();
+    document.getElementById('btn').classList.toggle('d-none');
+};
+
 function render(detailsAboutPokemons) {
     let mainContainerContentRef = document.getElementById('main_container');
-    mainContainerContentRef.innerHTML = detailsAboutPokemons.map((pokemon) => pokeCardsTemplate(pokemon)).join('');
+    mainContainerContentRef.innerHTML += detailsAboutPokemons.map((pokemon) => pokeCardsTemplate(pokemon)).join('');
 };
 
 function pokeCardsTemplate(pokemon) {
-      return `
+    return `
                <div class="poke-card bg-${pokemon.types[0].type.name}">
                <h3>${pokemon.name.toUpperCase()}</h3>
-               <img src="${pokemon.sprites.front_default}" alt="pokemon-pic">
+               <img class="poke-img"src="${pokemon.sprites.other.home.front_default}" alt="pokemon-pic">
                <p>${pokemon.types[0].type.name}</p>
                </div>
       `;
